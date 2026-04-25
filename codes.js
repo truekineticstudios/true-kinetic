42de2fedaf19a7a692b8bd80634d017ed69824b1
True Kinetic Studios <129095637+truekineticstudios@users.noreply.github.com>
2026-04-25 18:16:28 +0300

    Add code-link mappings for various resources

--- codes.js
+++ codes.js
@@ -1,1 +1,6 @@
-
+// Ayrı dosyada kod–link eşleştirmesi
+const codes = {
+  "ANTICHEAT-001": "https://dosya.co/93ydztbsok23/Copilot_20260418_203654.png.html",
+  "TRACKING-001": "https://seninsite.com/downloads/head_body_tracking.zip",
+  "LIGHTING-001": "https://seninsite.com/downloads/lighting_weather.zip"
+};
